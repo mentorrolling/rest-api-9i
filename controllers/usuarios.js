a8bb10b06edbd9f103a9340b65f4ee070ac6bb69
Pablo Marino <pmarino2013@gmail.com>
2021-08-04 21:56:32 -0300

    Configuracion base de datos y POST

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -1,4 +1,5 @@
 const { request, response } = require("express");
+const Usuario = require("../models/usuario");
 
 const usuariosGet = (req = request, res = response) => {
   res.json({
@@ -6,11 +7,19 @@ const usuariosGet = (req = request, res = response) => {
   });
 };
 
-const usuariosPost = (req = request, res = response) => {
+const usuariosPost = async (req = request, res = response) => {
+  const { nombre, email, password, rol } = req.body;
+
+  const usuario = new Usuario({ nombre, email, password, rol });
+
+  await usuario.save();
+
   res.json({
-    msg: "POST usuarios",
+    msg: "Usuario creado",
+    usuario,
   });
 };
+
 const usuariosPut = (req = request, res = response) => {
   res.json({
     msg: "PUT usuarios",
--- models/server.js
+++ models/server.js
@@ -2,16 +2,27 @@ const express = require("express");
 
 const cors = require("cors");
 
+//importar conexión a BD
+const { dbConnection } = require("../database/config");
+
 class Server {
   constructor() {
     //inicialicen cuando se levente el server
     this.app = express();
     this.usuariosPath = "/api/usuarios";
+
+    //conexion
+    this.conectarDB();
     //middlewares
     this.middlewares();
 
     //rutas
     this.routes();
+  }
+
+  //funcion para conectar la BD
+  async conectarDB() {
+    await dbConnection();
   }
 
   middlewares() {
@@ -20,6 +31,9 @@ class Server {
 
     //CORS
     this.app.use(cors());
+
+    //acceso al body leer y parsear
+    this.app.use(express.json());
   }
 
   routes() {
